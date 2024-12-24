596555c3a2edef2f82e0a8b04d7e8036bbdf925c
Igor Balandin <igor.balandin@gmx.de>
2024-12-24 09:21:51 +0100

    Handling Events
    modified:   src/components/ListGroup.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,30 +1,19 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisko", "Tokyo", "London", "Paris"];
-  items = [];
 
-  /*
-    const message = items.length === 0 ? <p>No item found</p> : null;
-
-    const getMessage = () => {
-        items.length === 0 ? <p>No item found</p> : null;
-    }
-  */
-
-  /*if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );*/
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
